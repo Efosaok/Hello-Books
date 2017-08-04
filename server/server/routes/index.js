f63339694896f1ce0d4d01210df448c93b8ccb7e
efosa <efosaokpugie@gmail.com>
2017-08-04 16:02:11 +0100

    made changes to my route files

--- server/server/routes/index.js
+++ server/server/routes/index.js
@@ -1,6 +1,14 @@
-const express = require("express");
-const app = express();
+const usersController = require('../controllers/users');
+const bookController = require('../controllers/books')
 
-app.get('/api', (req, res) => res.status(200).send({
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Todos API!',
   }));
+
+  app.post('/api/users/signup', usersController.create);
+  app.post("/api/users/signin", usersController.signin);
+  app.post('/api/books', bookController.addBook);
+  app.get('/api/books', bookController.getAvailableBooks);
+  app.put('/api/books/:bookid' ,bookController.modifyBook);
+};
--- server/server/routes/users.js
+++
@@ -1,5 +0,0 @@
-const usersController = require('../controllers').user
-
-module.exports = (app) => {
-  app.post('/api/signup', usersController.create);
-};
